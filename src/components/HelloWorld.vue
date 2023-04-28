f48f8d9e8cb1c60e94ce7b27280ea4b3128cc43d
D <107583272+5e@users.noreply.github.com>
2023-04-29 00:02:32 +0100

    added new icons and skills section

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -8,7 +8,11 @@
               <v-card-title>
                 <div>
                   <v-avatar size="90" class="elevation-2">
-                    <v-img :width="100" aspect-ratio="16/9" src="../assets/image1.png"></v-img>
+                    <v-img
+                      :width="100"
+                      aspect-ratio="16/9"
+                      src="../assets/image1.png"
+                    ></v-img>
                   </v-avatar>
                 </div>
                 Dominik Pawlowski
@@ -30,46 +34,74 @@
               BSc (Hons) in Digital and Technology Solutions
             </v-card-text>
             <v-card-text>
-              <div><b>Skills</b></div>
+              <div><b>Experience</b></div>
               <v-row>
-                <v-col cols="6">
+                <v-col cols="12">
                   4 years of experience building full stack applications using
                   Vue.js, C# and Microsoft SQL Server
-                  <br /><br />Experience with Python in numerous side projects
-                  <br /><br />Experience with agile/scrum development
-                  methodologies
+                  <br />Experience with Python in numerous side projects
+                  <br />Experience with agile/scrum development methodologies
                 </v-col>
-                <v-col cols="6" class="pt-0">
-                  <v-list density="compact" class="pt-0">
-                    <v-list-item v-for="item in items" :key="item.text" :title="item.text"
-                      :append-avatar="item.icon"></v-list-item>
-                  </v-list>
-                </v-col>
-
               </v-row>
-
+            </v-card-text>
+            <v-card-text>
+              <div><b>Skills</b></div>
+              <v-row>
+                <v-col
+                  cols="6"
+                  class="pb-0"
+                  v-for="item in items"
+                  :key="item.text"
+                >
+                  <v-sheet
+                    rounded
+                    class="d-flex px-3 py-2"
+                    color="grey-lighten-3"
+                  >
+                    <span
+                      >{{ item.text }}
+                      <br />
+                      <span style="font-size: 0.75rem">
+                        {{ item.description }}
+                      </span>
+                    </span>
+                    <v-spacer></v-spacer>
+                    <div class="align-self-center">
+                      <v-icon :icon="item.icon"></v-icon>
+                    </div>
+                  </v-sheet>
+                </v-col>
+              </v-row>
             </v-card-text>
             <v-card-actions>
-
               <v-scroll-y-transition mode="out-in">
                 <div class="mr-2" v-if="spotifyStatus != null">
-                  <v-icon style="color: #1DB954;" icon="mdi-spotify"></v-icon>
+                  <v-icon style="color: #1db954" icon="mdi-spotify"></v-icon>
                 </div>
               </v-scroll-y-transition>
               <v-scroll-y-transition mode="out-in">
-                <span style="line-height: 18px; font-size: 0.875rem; font-weight: 400;" v-if="spotifyStatus != null"
-                  :key="spotifyStatus.item.uri">{{
-                    spotifyStatus.item.name }}
-                  <br>
-                  <span style="font-size: 0.75rem; font-weight: 400;">{{ allArtists }}</span>
+                <span
+                  style="
+                    line-height: 18px;
+                    font-size: 0.875rem;
+                    font-weight: 400;
+                  "
+                  v-if="spotifyStatus != null"
+                  :key="spotifyStatus.item.uri"
+                  >{{ spotifyStatus.item.name }}
+                  <br />
+                  <span style="font-size: 0.75rem; font-weight: 400">{{
+                    allArtists
+                  }}</span>
                 </span>
               </v-scroll-y-transition>
               <v-spacer></v-spacer>
-              <v-btn target="_blank" href="https://www.linkedin.com/in/dominik-p-45036a269/">LinkedIn</v-btn>
-
-
+              <v-btn
+                target="_blank"
+                href="https://www.linkedin.com/in/dominik-p-45036a269/"
+                >LinkedIn</v-btn
+              >
             </v-card-actions>
-
           </v-card>
         </v-col>
       </v-row>
@@ -78,7 +110,7 @@
 </template>
 
 <script>
-import axios from 'axios'
+import axios from "axios";
 
 export default {
   data() {
@@ -87,27 +119,38 @@ export default {
       items: [
         {
           text: "Vue.js",
-          icon: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/2367px-Vue.js_Logo_2.svg.png",
+          icon: "fa:fas fa-brands fa-vuejs",
+          description: "4 years",
         },
         {
           text: "Vuetify",
-          icon: "https://iconape.com/wp-content/png_logo_vector/vuetify.png",
+          icon: "mdi-vuetify",
+          description: "4 years",
         },
         {
-          text: "C#",
-          icon: "https://e7.pngegg.com/pngimages/328/221/png-clipart-c-programming-language-logo-microsoft-visual-studio-net-framework-javascript-icon-purple-logo.png",
+          text: ".NET C#",
+          icon: "mdi-dot-net",
+          description: "4 years",
         },
         {
           text: "SQL",
-          icon: "https://cdn-icons-png.flaticon.com/512/4299/4299956.png",
+          icon: "mdi-database-search",
+          description: "5 years",
         },
         {
           text: "REST APIs",
-          icon: "https://toppng.com/uploads/preview/rest-api-icon-rest-api-icon-11553510526uqs2ynyga2.png",
+          icon: "mdi-cloud-braces",
+          description: "4 years",
         },
         {
           text: "Python",
-          icon: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/1200px-Python-logo-notext.svg.png",
+          icon: "mdi-language-python",
+          description: "2 years",
+        },
+        {
+          text: "JavaScript",
+          icon: "fa:fas fa-brands fa-square-js",
+          description: "5 years",
         },
       ],
     };
@@ -115,33 +158,34 @@ export default {
   computed: {
     allArtists() {
       var total = "";
-      this.spotifyStatus.item.artists.forEach(artist => {
+      this.spotifyStatus.item.artists.forEach((artist) => {
         total += artist.name;
-        total += ", "
+        total += ", ";
       });
       total = total.slice(0, -2);
       return total;
-    }
+    },
   },
   methods: {
     getSpotifyStatus() {
       this.recipesLoading = true;
-      axios.get('https://cloudflare-spotify.5egt.workers.dev/song')
+      axios
+        .get("https://cloudflare-spotify.5egt.workers.dev/song")
         .then((response) => {
           if (response.status != 204) {
             this.spotifyStatus = response.data;
           } else {
             this.spotifyStatus = null;
           }
-        })
+        });
     },
   },
   mounted() {
     this.getSpotifyStatus();
 
-    const timer = setInterval(() => {
-      this.getSpotifyStatus();
-    }, 5000);
+    // const timer = setInterval(() => {
+    //   this.getSpotifyStatus();
+    // }, 5000);
   },
 };
 </script>
