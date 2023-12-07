e8badb81417fc08858e1272db0d4b0b49a01f8b5
5e <107583272+5e@users.noreply.github.com>
2023-12-07 00:00:27 +0000

    blog views bug fix

--- pages/blog/[...slug].vue
+++ pages/blog/[...slug].vue
@@ -1,32 +1,30 @@
 <template>
   <div>
-    <ContentDoc v-slot="{ doc }">
-      <article>
-        <h1 :key="doc.id">{{ doc.header }}</h1>
-
-        <div class="mb-3">
-          <div class="float-right text-grey">
-            <v-icon icon="mdi-eye-outline" size="x-small"></v-icon>
-            {{ views }}
-          </div>
-          <p class="text-grey">{{ doc.createdAt }}</p>
+    <article>
+      <h1 :key="doc.id">{{ doc.header }}</h1>
+      <div class="mb-3">
+        <div class="float-right text-grey">
+          <v-icon icon="mdi-eye-outline" size="x-small"></v-icon>
+          {{ views }}
         </div>
-        <ContentRenderer :value="doc" />
-        <BlogComments :blog-id="doc.id" />
-      </article>
-    </ContentDoc>
+        <p class="text-grey">{{ doc.createdAt }}</p>
+      </div>
+      <ContentRenderer :value="doc" />
+      <BlogComments :blog-id="doc.id" />
+    </article>
   </div>
 </template>
 
 <script setup>
 const views = ref(0);
+const { path } = useRoute();
+const doc = await queryContent(path).findOne();
 const supabase = useSupabaseClient();
 
-async function addView(id) {
+async function addView() {
   const { data, error } = await supabase.rpc("addview", {
-    blogid: id,
+    blogid: doc.id,
   });
-  views.value += 1;
 }
 
 async function getViews() {
@@ -34,13 +32,13 @@ async function getViews() {
   const { data } = await client
     .from("posts")
     .select("views")
-    .eq("id", 2)
+    .eq("id", doc.id)
     .single();
-  views.value = data.views;
+  views.value = data.views + 1;
 }
 
 onMounted(() => {
   getViews();
-  // addView();
+  addView();
 });
 </script>
