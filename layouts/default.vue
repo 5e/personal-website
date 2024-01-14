72520901186e7b071b3508d1430636d560b6f84c
5e <107583272+5e@users.noreply.github.com>
2024-01-14 00:14:04 +0000

    removed footer

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,13 +1,20 @@
 <template>
   <v-row no-gutters>
-    <v-col xl="4" offset-xl="4" lg="6" offset-lg="3" sm="10" offset-sm="1" xs="10" offset-xs="1">
+    <v-col
+      xl="4"
+      offset-xl="4"
+      lg="6"
+      offset-lg="3"
+      sm="10"
+      offset-sm="1"
+      xs="10"
+      offset-xs="1"
+    >
       <AppBar></AppBar>
       <div class="px-5">
         <slot />
       </div>
-      <AppFooter></AppFooter>
     </v-col>
-
   </v-row>
 </template>
 
