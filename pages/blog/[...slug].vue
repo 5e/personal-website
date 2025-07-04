aad7cff3c9064590ce22588a283bb82b6bb703f3
5e <107583272+5e@users.noreply.github.com>
2025-07-04 22:32:13 +0100

    remove comments

--- pages/blog/[...slug].vue
+++ pages/blog/[...slug].vue
@@ -3,14 +3,9 @@
     <article>
       <h1 :key="doc.id">{{ doc.header }}</h1>
       <div class="mb-3">
-        <div class="float-right text-grey">
-          <v-icon icon="mdi-eye-outline" size="x-small"></v-icon>
-          {{ views }}
-        </div>
         <p class="text-grey">{{ doc.createdAt }}</p>
       </div>
       <ContentRenderer :value="doc" />
-      <BlogComments :blog-id="doc.id" />
     </article>
   </div>
 </template>
@@ -19,26 +14,4 @@
 const views = ref(0);
 const { path } = useRoute();
 const doc = await queryContent(path).findOne();
-const supabase = useSupabaseClient();
-
-async function addView() {
-  const { data, error } = await supabase.rpc("addview", {
-    blogid: doc.id,
-  });
-}
-
-async function getViews() {
-  const client = useSupabaseClient();
-  const { data } = await client
-    .from("posts")
-    .select("views")
-    .eq("id", doc.id)
-    .single();
-  views.value = data.views + 1;
-}
-
-onMounted(() => {
-  getViews();
-  addView();
-});
 </script>
